1c541537edfe6cf19469683a5d4ebc58f9a2072d
Richard <rzhou564@terpmail.umd.edu>
2021-03-16 18:50:12 -0700

    changed back to post once a day

--- schedule.py
+++ schedule.py
@@ -4,7 +4,7 @@ from twitter_analyzer import BotFunctions
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=3) #('cron', hour = 23, minute = 59, second = 59, timezone = utc) # 23:59:59 UTC
+@sched.scheduled_job('cron', hour = 23, minute = 59, second = 59, timezone = utc) # 23:59:59 UTC
 def scheduled_job():
     BotFunctions().post_once_a_day()
 
